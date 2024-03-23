a964d6e3a6b4a405db5d661d87a76ce538f3d82a
Anderson <anderson052594@gmail.com>
2024-03-22 19:13:52 -0500

    Models addition

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=800)
+    date = models.DateTimeField(auto_now_add=True)
+    # image = models.ImageField(upload_to='images/')
